b678865bfecb01cc7c80d3cecb7f6fe578cc3147
itsroshravi <112476732+itsroshravi@users.noreply.github.com>
2025-02-14 11:10:27 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("*********************************");
+        System.out.println("Hello from Github");
+        System.out.println("*********************************");
     }
 }
